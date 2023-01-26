5c1e1397222a5a434796b2986b854aadc6fb5498
Abdulloh <abdulloxcobra@gmail.com>
2023-01-26 19:58:53 +0500

    tests

--- test.py
+++ test.py
@@ -12,7 +12,29 @@ client = TelegramClient('agregat', API_ID, API_HASH)
 
 @client.on(events.NewMessage(incoming=True))
 async def my_event_handler(event):
-    print(event.chat_id)
+    print(event.peer_id)
+    try:
+        if event.peer_id.user_id != None:
+            await event.forward_to(-835631089)
+            await client.send_message(-835631089, 'it\'s from a user')
+            print('it\'s  a user')
+    except: 
+        try:
+            if event.peer_id.chat_id != None:
+                await event.forward_to(-835631089)
+                await client.send_message(-835631089, 'it\'s from a group')
+                print("It's from a chat ")
+        except:
+            try:
+                if event.peer_id.channel_id != None:
+                    await event.forward_to(-835631089)
+                    await client.send_message(-835631089, 'it\'s from a channel')
+                    print("It's from a channel")
+            except:
+                pass
+
+    
+    
 
 client.start()
 client.run_until_disconnected()
