a9f217b6990b21288dac98e26556270d0e61f961
Abdulloh <abdulloxcobra@gmail.com>
2023-01-28 18:00:31 +0500

    fix

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import os
+from traceback import print_exc
 from dotenv import load_dotenv
 from telethon import TelegramClient, events
+import logging
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.INFO)
 
 load_dotenv()
 
@@ -10,28 +15,53 @@ API_HASH = os.getenv("API_HASH")
 # The first parameter is the .session file name (absolute paths allowed)
 client = TelegramClient('agregat', API_ID, API_HASH)
 
-@client.on(events.NewMessage(incoming=True))
+@client.on(events.Album(chats=-861607927, blacklist_chats=True))
+async def albums(event):
+    await event.forward_to(-861607927)
+    raise events.StopPropagation
+
+
+
+@client.on(events.NewMessage(incoming=True, chats=-861607927, blacklist_chats=True))
 async def my_event_handler(event):
-    print(event.peer_id)
-    try:
-        if event.peer_id.user_id != None:
-            await event.forward_to(-835631089)
-            await client.send_message(-835631089, 'it\'s from a user')
-            print('it\'s  a user')
-    except: 
+    if event.is_private:
+        user = await client.get_entity(event.peer_id)
+        first = user.first_name
+        last = user.last_name if not "None" else ""
+        await client.send_message(-861607927, f"Сообщение с пользователя {first} {last}")
+        # if await event.media:
+        #     await client.send
+        await event.forward_to(-861607927)
+        print('it\'s  a user')
+    else:
+        group = await client.get_entity(event.peer_id)
         try:
-            if event.peer_id.chat_id != None:
-                await event.forward_to(-835631089)
-                await client.send_message(-835631089, 'it\'s from a group')
-                print("It's from a chat ")
+            user = await client.get_entity(event.from_id)        
+            first = user.first_name
+            last = user.last_name if not "None" else ""
+            link=f"{first} {last}"
+        except:
+            print_exc()
+            link = "username"
+        try:
+            link = f"<a href=https://t.me/+{user.phone}>{first} {last}</a>"
         except:
             try:
-                if event.peer_id.channel_id != None:
-                    await event.forward_to(-835631089)
-                    await client.send_message(-835631089, 'it\'s from a channel')
-                    print("It's from a channel")
+                link = f"<a href=https://t.me/{user.username}>{first} {last}</a>"
             except:
                 pass
+        group_link = f'{group.title}'
+        try:
+            group_link = f'<a href=https://t.me/{group.username}>{group.title}</a>'
+        except:
+            pass
+        if event.message.text and event.message.media == None:
+            await client.send_message(-861607927, f"Сообщение от пользователя {link}, группы {group_link}\n{event.message.text}", parse_mode="Html")
+        elif event.message.media:
+            print(event.message.photo)
+            await client.send_message(-861607927, f"Сообщение от пользователя {link}, группы {group_link}", parse_mode="Html")      
+
+    raise events.StopPropagation
 
     
     
---
+++ testing.py
@@ -0,0 +1,48 @@
+
+from collections import Counter 
+  
+# counts word frequency
+def count_words(text):                  
+    skips = [".", ", ", ":", ";", "'", '"', "\n", '\xa0'] 
+    for ch in skips: 
+        text = text.replace(ch, " ") 
+    word_counts = {} 
+    for word in text.split(" "): 
+        if word in word_counts: 
+            word_counts[word]+= 1 
+        else: 
+            word_counts[word]= 1 
+    return word_counts 
+  
+    # >>>count_words(text) You can check the function 
+  
+# counts word frequency using
+# Counter from collections 
+def count_words_fast(text):     
+    text = text.lower() 
+    skips = [".", ", ", ":", ";", "'", '"', '\n'] 
+    for ch in skips: 
+        text = text.replace(ch, " ") 
+    word_counts = Counter(text.split(" ")) 
+    return word_counts 
+  
+    # >>>count_words_fast(text) You can check the function 
+
+txt = """
+[ Album ]
+Diqqat sotiladi
+🦬  Buqacha yeb ichiwi zoʻr sogʻlom
+💵Narxi: 10 mln keliwamiz
+🏠Manzil: Parkent tuamni ( Tambalak )
+Tel:📲 +998943678171
+
+
+
+
+
+Admin @FURQAT_T
+Bizning kanal
+@Parkent_molbozor
+Dostlarga ulawing
+"""
+print(count_words(text=txt))
