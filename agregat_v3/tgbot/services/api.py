cccc79f1bb5d308b2eea893cca62dcb07d283277
Abdulloh Alimv <raistlinmajere10@gmail.com>
2023-05-29 17:26:47 +0500

    changes in api

--- agregat_v3/tgbot/services/api.py
+++ agregat_v3/tgbot/services/api.py
@@ -12,7 +12,9 @@ def write_to_file(text, filename, encoding="utf-8"):
         f.write(text)
 
 
-async def register_new_product_and_user(phone_number, media_files, datetime, got_data: dict, categories=[], status="1"):
+async def register_new_product_and_user(
+    phone_number, media_files, datetime, got_data: dict, categories=[], status="1"
+):
     """
     Registers a new product and user along with their information.
 
@@ -29,42 +31,46 @@ async def register_new_product_and_user(phone_number, media_files, datetime, got
     """
     url = "http://62.209.129.42/product/"
 
-    payload = json.dumps({
-    "product_user": {
-        "user_id": int(got_data['user_id']),
-        "user_name": f"{got_data['user_name']}",
-        "user_link": f"{got_data['user_link']}",
-        "phone_number": f"{phone_number}"
-    },
-    "category": list(categories),
-    "group_id": int(got_data['group_id']),
-    "group_name": f"{got_data['group_name']}",
-    "group_link": f"{got_data['group_link']}",
-    "message_id": int(got_data['message_id']),
-    "message_text": f"{got_data['message_text']}",
-    "media_file": f"{media_files}",
-    "datatime": datetime,
-    "status": status,
-    })
-    headers = {
-    'Content-Type': 'application/json'
-    }
+    payload = json.dumps(
+        {
+            "product_user": {
+                "user_id": int(got_data["user_id"]),
+                "user_name": f"{got_data['user_name']}",
+                "user_link": f"{got_data['user_link']}",
+                "phone_number": f"{phone_number}",
+            },
+            "category": list(categories),
+            "group": {
+                "group_id": int(got_data["group_id"]),
+                "group_name": f"{got_data['group_name']}",
+                "group_link": f"{got_data['group_link']}",
+            },
+            "message_id": int(got_data["message_id"]),
+            "message_text": f"{got_data['message_text']}",
+            "media_file": f"{media_files}",
+            "datatime": datetime,
+            "status": status,
+        }
+    )
+    headers = {"Content-Type": "application/json"}
 
     async with aiohttp.ClientSession() as session:
         async with session.post(url, headers=headers, data=payload) as response:
-            logging.critical('product added')
+            logging.critical("product added")
             try:
+                logging.error(await response.json())
                 return await response.json()
             except:
                 write_to_file(await response.text(), "last_error.txt")
-                logging.critical('An CRITICAL error occured while adding product')
+                logging.critical("An CRITICAL error occured while adding product")
+
 
 async def delete_product(message_id, group_id):
     url = f"http://62.209.129.42/product/?group_id={group_id}&message_id={message_id}"
 
     async with aiohttp.ClientSession() as session:
         async with session.delete(url) as response:
-            logging.critical('product deleted')
+            logging.critical("product deleted")
             return response.status
 
 
@@ -73,23 +79,16 @@ async def get_product(group_id, message_id):
 
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
-            logging.critical('product fetched')
+            logging.critical("product fetched")
             return await response.json()
 
 
 async def update_product(message_id, group_id, categories: list):
     url = f"http://62.209.129.42/update_by_group_id/?group_id={group_id}&message_id={message_id}"
-    payload = json.dumps({
-        "category": categories
-    })
-    headers = {
-        'Content-Type': 'application/json'
-    }
+    payload = json.dumps({"category": categories})
+    headers = {"Content-Type": "application/json"}
 
     async with aiohttp.ClientSession() as session:
         async with session.patch(url, headers=headers, data=payload) as response:
-            logging.critical('product updated')
+            logging.critical("product updated")
             return await response.json()
-
-
-
